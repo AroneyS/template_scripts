eafe655c1379ac5013115dd6c216898bb4a6a83e
AroneyS <samuel.aroney@qut.edu.au>
2021-11-29 15:15:10 +1000

    add extra options

--- template_chunk_script.sh
+++ template_chunk_script.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
+# envionment: 
 
 BASENAME=$(echo $0 | sed "s=.*/==" | sed "s/\.sh//")
-COMMAND_FILE=${BASENAME}_commands
 CPUS=1
 MEMORY=4
 NCHUNKS=50
 
-while getopts 'i:o:c:m:n:' flag; do
+while getopts 'i:o:c:m:n:s:t:' flag; do
   case "${flag}" in
     i) INPUT_FILE_LIST="${OPTARG}" ;;
-    o) OUTPUT_DIR="${OPTARG}/subdir" ;;
+    o) OUTPUT_DIR="results/subdir/${OPTARG}" ;;
     c) CPUS="${OPTARG}" ;;
     m) MEMORY="${OPTARG}" ;;
     n) NCHUNKS="${OPTARG}" ;;
-    *) echo "Usage: -i input_file_list -o output_dir [-c cpus -m memory -n num_chunks]"
+    s) SUFFIX="${OPTARG}" ;;
+    t) HOURS="${OPTARG}" ;;
+    *) echo "Usage: -i input_file_list -o output_dir [-c cpus -m memory -n num_chunks -s suffix -t time_hours]"
        exit 1 ;;
   esac
 done
 
+BASENAME=${BASENAME}$SUFFIX
+COMMAND_FILE=${BASENAME}_commands
 mkdir -p $OUTPUT_DIR/logs
 
 cat $INPUT_FILE_LIST | parallel --dryrun \
@@ -26,7 +30,7 @@ cat $INPUT_FILE_LIST | parallel --dryrun \
   '&>' $OUTPUT_DIR/logs/{/.}.log \
   > $COMMAND_FILE
 
-mqsub --name $BASENAME --command-file $COMMAND_FILE --chunk-num $NCHUNKS --mem $MEMORY --cpus $CPUS \
+mqsub --name $BASENAME --command-file $COMMAND_FILE --chunk-num $NCHUNKS --mem $MEMORY --cpus $CPUS --hours $HOURS \
   &> $OUTPUT_DIR/logs/${BASENAME}.log
 
 # find ./ -name "${BASENAME}*e*" | parallel cat
