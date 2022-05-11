7abffa41354d9e7fc6b97aa41bd271b61ee2d142
AroneyS <samuel.aroney@qut.edu.au>
2022-05-12 09:27:34 +1000

    update python template

--- template_python.py
+++ template_python.py
@@ -1,8 +1,23 @@
-###############
-### name.py ###
-###############
-# Author: Samuel Aroney
-# Description
+#!/usr/bin/env python3
 
-import csv
+"""
+Author: Samuel Aroney
+Description
+"""
 
+import os
+import sys
+import argparse
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--input", type=argparse.FileType("r"), metavar="<INPUT>", help="Input file")
+    parser.add_argument("-o", "--outfile", type=argparse.FileType("w"), default=sys.stdout, help="Output file")
+
+    args = parser.parse_args(arguments)
+
+    print(args)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
