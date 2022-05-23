d0a5ca4a885746c4543bb60ea9522492b54851b4
AroneyS <samuel.aroney@qut.edu.au>
2022-05-23 14:59:59 +1000

    add logging to template

--- template_python.py
+++ template_python.py
@@ -8,16 +8,29 @@ Description
 import os
 import sys
 import argparse
+import logging
 
 
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--debug', help='output debug information', action="store_true")
+    parser.add_argument('--quiet', help='only output errors', action="store_true")
+
     parser.add_argument("--input", type=argparse.FileType("r"), metavar="<INPUT>", help="Input file")
     parser.add_argument("-o", "--outfile", type=argparse.FileType("w"), default=sys.stdout, help="Output file")
 
     args = parser.parse_args(arguments)
 
-    print(args)
+    # Setup logging
+    if args.debug:
+        loglevel = logging.DEBUG
+    elif args.quiet:
+        loglevel = logging.ERROR
+    else:
+        loglevel = logging.INFO
+    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    logging.info(args)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
