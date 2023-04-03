f3fff20d373f8e6404e660a04ccc50dd22bddccd
AroneyS <samuel.aroney@qut.edu.au>
2023-04-03 15:28:27 +1000

    add -l argument to switch to local running

--- template_chunk_script.sh
+++ template_chunk_script.sh
@@ -9,8 +9,9 @@ MEMORY=8
 NCHUNKS=1
 HOURS=168
 QUEUE=microbiome
+SUBMIT=true
 
-while getopts 'i:o:c:m:n:s:t:q:' flag; do
+while getopts 'i:o:c:m:n:s:t:q:l' flag; do
   case "${flag}" in
     i) INPUT_FILE_LIST="${OPTARG}" ;;
     o) OUTPUT_DIR="results/subdir/${OPTARG}" ;;
@@ -20,14 +21,15 @@ while getopts 'i:o:c:m:n:s:t:q:' flag; do
     s) SUFFIX="${OPTARG}" ;;
     t) HOURS="${OPTARG}" ;;
     q) QUEUE="${OPTARG}" ;;
-    *) echo "Usage: -i input_file_list -o output_dir [-c cpus -m memory -n num_chunks -s suffix -t time_hours -q queue]"
+    l) SUBMIT="" ;;
+    *) echo "Usage: -i input_file_list -o output_dir [-c cpus -m memory -n num_chunks -s suffix -t time_hours -q queue -l (local run)]"
        exit 1 ;;
   esac
 done
 
 BASENAME=${BASENAME}$SUFFIX
 mkdir -p $OUTPUT_DIR/logs
-COMMAND_FILE=$OUTPUT_DIR/logs/${BASENAME}_commands
+COMMAND_FILE=$OUTPUT_DIR/logs/${BASENAME}_commands.sh
 
 cat $INPUT_FILE_LIST | parallel --dryrun --plus --col-sep "\t" \
   run_something \
@@ -35,7 +37,12 @@ cat $INPUT_FILE_LIST | parallel --dryrun --plus --col-sep "\t" \
   '&>' $OUTPUT_DIR/logs/{/.}.log \
   > $COMMAND_FILE
 
-mqsub -q $QUEUE --name $BASENAME --command-file $COMMAND_FILE --chunk-num $NCHUNKS --mem $MEMORY --cpus $CPUS --hours $HOURS \
-  &> $OUTPUT_DIR/logs/${BASENAME}.log
+if [ -z "$SUBMIT" ]
+then
+  echo "Run commands in $COMMAND_FILE"
+else
+  mqsub -q $QUEUE --name $BASENAME --command-file $COMMAND_FILE --chunk-num $NCHUNKS --mem $MEMORY --cpus $CPUS --hours $HOURS \
+    &> $OUTPUT_DIR/logs/${BASENAME}.log
+fi
 
 # find ./ -maxdepth 1 -name "${BASENAME}*e*" | parallel cat
