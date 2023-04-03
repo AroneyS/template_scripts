5662c1f0217105ff9cdee3ba086e72eb75db3488
AroneyS <samuel.aroney@qut.edu.au>
2023-04-03 10:15:27 +1000

    swtich to double quotes

--- template_python.py
+++ template_python.py
@@ -13,8 +13,8 @@ import logging
 
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--debug', help='output debug information', action="store_true")
-    parser.add_argument('--quiet', help='only output errors', action="store_true")
+    parser.add_argument("--debug", help="output debug information", action="store_true")
+    parser.add_argument("--quiet", help="only output errors", action="store_true")
 
     parser.add_argument("--input", type=argparse.FileType("r"), help="Input file")
     parser.add_argument("-o", "--outfile", type=argparse.FileType("w"), default=sys.stdout, help="Output file")
@@ -28,7 +28,7 @@ def main(arguments):
         loglevel = logging.ERROR
     else:
         loglevel = logging.INFO
-    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p')
+    logging.basicConfig(level=loglevel, format="%(asctime)s %(levelname)s: %(message)s", datefmt="%Y/%m/%d %I:%M:%S %p")
 
     logging.info(args)
 
